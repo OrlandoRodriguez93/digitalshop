4f9d55a7904bf61888a5ccf91e47ca83f3433197
jlrodriguezl <orlando.rodriguez93@hotmail.com>
2022-08-08 13:02:17 -0500

    commit

--- service-customer/src/main/resources/data.sql
+++ service-customer/src/main/resources/data.sql
@@ -1,10 +1,10 @@
-INSERT INTO tbl_regions (name) VALUES ('Sudamérica');
-INSERT INTO tbl_regions (name) VALUES ('Centroamérica');
-INSERT INTO tbl_regions (name) VALUES ('Norteamérica');
-INSERT INTO tbl_regions (name) VALUES ('Europa');
-INSERT INTO tbl_regions (name) VALUES ('Asia');
-INSERT INTO tbl_regions (name) VALUES ('Africa');
-INSERT INTO tbl_regions (name) VALUES ('Oceanía');
-INSERT INTO tbl_regions (name) VALUES ('Antártida');
+INSERT INTO tbl_regions (id, name) VALUES (1, 'Sudamérica');
+INSERT INTO tbl_regions (id, name) VALUES (2, 'Centroamérica');
+INSERT INTO tbl_regions (id, name) VALUES (3, 'Norteamérica');
+INSERT INTO tbl_regions (id, name) VALUES (4, 'Europa');
+INSERT INTO tbl_regions (id, name) VALUES (5, 'Asia');
+INSERT INTO tbl_regions (id, name) VALUES (6, 'Africa');
+INSERT INTO tbl_regions (id, name) VALUES (7, 'Oceanía');
+INSERT INTO tbl_regions (id, name) VALUES (8, 'Antártida');
 
-INSERT INTO tbl_customers (number_id,first_name,last_name , email, photo_url, region_id, state) VALUES('32404580', 'Andrés', 'Guzmán', 'profesor@bolsadeideas.com','',1,'CREATED');
+INSERT INTO tbl_customers (id,number_id,first_name,last_name , email, photo_url, region_id, state) VALUES(1,'32404580', 'Andrés', 'Guzmán', 'profesor@bolsadeideas.com','',1,'CREATED');
