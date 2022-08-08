9b88c286c5145b5ac6224e2fb134edc0f9b2fab3
jlrodriguezl <orlando.rodriguez93@hotmail.com>
2022-08-08 14:04:56 -0500

    commit

--- config-service/src/main/java/com/example/store/config/ConfigServiceApplication.java
+++ config-service/src/main/java/com/example/store/config/ConfigServiceApplication.java
@@ -1,10 +1,12 @@
 package com.example.store.config;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
+@EnableAutoConfiguration
 @SpringBootApplication
 public class ConfigServiceApplication {
 
